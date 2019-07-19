3b53fc19d4e47fa6ee41e66bd6cec6dd135e9f14
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-19 11:55:04 -0400

    First commit; just simple heading and paragraph

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is an React App</h1>
+      <p>This is really working!</p>
+    </div>
+  );
+}
+
+export default App;
+ 

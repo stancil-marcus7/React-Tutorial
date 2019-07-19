aaf7a0e2351557a41ed933359f96d8e911177841
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-19 14:43:58 -0400

    Add dynamic content for person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//You can omit the .js at the end
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
       <h1>This is an React App</h1>
       <p>This is really working!</p>
+      <Person name="Marcus" age="21"/>
+      <Person name="Alex" age="13"/>
+      <Person name="George" age="100"/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+//ES6 version of component
+//Dont need import from component because we are exporting a function and not a compnent
+const person = (props) => {
+    //Returning JSX with paragraph that says "I'm a person"
+    //Out put dynamic content like the random number for the age of the person
+    //In class based components it would be this.props
+    //Must use parenthesis to return multiple lines
+    return(
+        <p>I'm {props.name} and I'm {props.age} years old</p>
+    )
+}
+
+export default person;

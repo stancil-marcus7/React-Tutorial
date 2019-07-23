571ee8729cf3288f8181f091473195080c49dc99
Marcus Stancil <stancil.marcus@yahoo.com>
2019-07-23 16:45:30 -0400

    Add aux import to render adjacent components

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 //Added import from Person.css
 import classes from './Person.module.css'
+import Aux from '../../../hoc/Aux';
 
 //ES6 version of component
 //Dont need import from component because we are exporting a function and not a compnent
@@ -13,14 +14,14 @@ class Person extends Component{
     //In class based components it would be this.props
     //Must use parenthesis to return multiple lines
     return(
-        <div className={classes.Person}>
+        <Aux>
             <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
             {/* Props.children accesses anything within the JSX elements */}
             {/* onClick is the event listener and it uses props.click from the person compnent in app.js to access switchNameHandler */}
             <p>{this.props.children}</p>
             {/* Included value={props.name} so that we can see the names of the person's in the input boxes */}
             <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </div>
+            </Aux>
     )
     }
     
---
+++ src/hoc/Aux.js
@@ -0,0 +1,8 @@
+//Works without import
+
+
+//This is a higher order component that we can use so that we can have adjacent components
+const aux = props => props.children;
+
+export default aux;
+    
